79429310378289940926ff6bdf60f1e4aed83deb
srunoudomsambath <toyroith@gmail.com>
2025-08-24 16:32:20 +0700

    Finalize: SETEC FINAL

--- js/script.js
+++ js/script.js
@@ -64,3 +64,10 @@ document.getElementById("showMoreBtn").addEventListener("click", function() {
 document.querySelector(".login").addEventListener("click", function() {
     window.location.href = "/login.html";
 });
+
+  const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
